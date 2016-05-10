c5e7f265400807df8a98ffc4a2a21658d7a1a617
Carla Babadopulos <cacababa@gmail.com>
2016-05-10 12:02:27 +0200

    Created tests for routing to #index and show for PostsController

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,8 @@
+class PostsController < ApplicationController
+  # GET /posts
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :posts, only: [:show, :create]
 
-  root to: "home#index"
+  root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
