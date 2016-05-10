0b63983a2aabfcb1b8c43ab82e0b89c1a88d6ac3
Carla Babadopulos <cacababa@gmail.com>
2016-05-10 15:04:32 +0200

    Created RSpec to the Post index view

---
+++ spec/views/posts/index.html.erb_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe "posts/index", type: :view do
+  let(:user) { User.create(email: "user@social.com", password: "12345678") }
+  let(:other_user) { User.create(email: "other-user@social.com", password: "12345678") }
+
+  before(:each) do
+    assign(:posts, [
+      Post.create!(
+        title: "Hey You",
+        message: "Hello, I'm writing tests!",
+        user: user
+      ),
+      Post.create!(
+        title: "Hey You too",
+        message: "Hi, Here's another post!",
+        user: other_user
+      )
+    ])
+
+    render
+  end
+
+  it "renders the title of the page" do
+    assert_select "h1", text: "Posts", count: 1
+  end
+
+  it "renders a list of title posts" do
+    assert_select ".post>h1", text: "Hey You", count: 1
+    assert_select ".post>h1", text: "Hey You too", count: 1
+  end
+
+  it "renders a list of posts" do
+    assert_select ".post>p", text: "Hello, I'm writing tests!", count: 1
+    assert_select ".post>p", text: "Hi, Here's another post!", count: 1
+  end
+
+  it "shows the user's email with the post" do
+    assert_select ".post>.meta", text: user.email, count: 1
+    assert_select ".post>.meta", text: other_user.email, count: 1
+  end
+end
