20e42172dbdd0ae01db8d6cb1bcf14d72bf875fd
Carla Babadopulos <cacababa@gmail.com>
2016-05-10 13:53:46 +0200

    Created tests to Post index and Post create controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,26 @@
 class PostsController < ApplicationController
   # GET /posts
   def index
+    @posts = Post.all
   end
 
   def show
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+    if @post.save
+      redirect_to root_url, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:message)
   end
 end
