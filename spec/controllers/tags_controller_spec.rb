991d9d02d3c77511fb147153e88fc58a79c38af8
Carla Babadopulos <cacababa@gmail.com>
2016-05-17 16:41:53 +0200

    Created rspec for tag controller

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 RSpec.describe TagsController, type: :controller do
 
+  let!(:tag) { Tag.create!(name: "Nice") }
+
   describe "GET #index" do
-    it "returns http success" do
+    it "assigns all tags as @tags" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(assigns(:tags)).to eq([tag])
     end
   end
 
   describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+    it "assigns @tag and render the show template" do
+      get :show, id: tag.id
+      expect(assigns(:tag)).to eq(tag)
     end
   end
 
