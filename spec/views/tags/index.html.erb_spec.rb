293bc9919884a204ea46408bc8fd0aa98f68d2c1
Carla Babadopulos <cacababa@gmail.com>
2016-05-18 16:21:31 +0200

    Created rspec to tag index view

--- spec/views/tags/index.html.erb_spec.rb
+++ spec/views/tags/index.html.erb_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "tags/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "tags/index", type: :view do
+
+  before(:each) do
+    assign(:tags, [
+      Tag.create!(
+        name: "Nice Tag"
+      ),
+      Tag.create!(
+        name: "Other Tag"
+      )])
+
+    assign(:tag, Tag.new)
+
+    render
+  end
+
+  it "renders the tag name" do
+    assert_select "h1", text: "Tags", count: 1
+  end
+
+  it "renders the tags" do
+    assert_select "#tagname", count:2
+  end
+
 end
